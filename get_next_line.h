6f8c8fdf0046be706299fc04defaf5c4057a98d4
Henrique de Campos Duller <henriqueduller@hotmail.com>
2021-06-22 11:58:43 -0300

    Mandatory/Bonus done. Norm compliant

--- get_next_line.h
+++ get_next_line.h
@@ -4,18 +4,11 @@
 # include <stdlib.h>
 # include <stddef.h>
 
-typedef struct s_list
-{
-	void			*content;
-	struct s_list	*next;
-}	t_list;
-
 void	*ft_calloc(size_t count, size_t size);
 size_t	ft_strlen(const char *s);
 char	*pre_break(char *str);
 char	*pos_break(char *str);
 int		has_break(char *p);
-
 int		get_next_line(int fd, char **line);
 
 #endif
